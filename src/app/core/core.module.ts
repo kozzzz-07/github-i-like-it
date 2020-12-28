d6fb5179f864c4970d90d07628e51508599b5c8b
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2020-12-28 15:18:40 +0900

    example の枠作成

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,11 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterModule],
   declarations: [MainNavComponent],
   exports: [MainNavComponent],
 })
