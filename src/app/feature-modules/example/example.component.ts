5521d373fa666dd6b9920fd934b75db2f12a6acf
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-01-04 01:43:40 +0900

    firebase auth の例

--- src/app/feature-modules/example/example.component.ts
+++ src/app/feature-modules/example/example.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
-  styleUrls: ['./example.component.scss']
+  styleUrls: ['./example.component.scss'],
 })
 export class ExampleComponent implements OnInit {
+  constructor(private auth: AngularFireAuth) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async login(): Promise<void> {
+    const provider = new firebase.auth.GithubAuthProvider();
+    provider.addScope('repo');
+
+    try {
+      const userCredential = await this.auth.signInWithPopup(
+        provider
+      );
+
+      const avatarUrl = (userCredential.additionalUserInfo?.profile as Profile).avatar_url;
+      const username = userCredential.additionalUserInfo?.username;
+
+      // MEMO: 型が合わないので強制変換
+      const additionalUserInfo = userCredential.additionalUserInfo as unknown as OAuthCredential;
+      const token = additionalUserInfo.accessToken;
+
+
+    } catch (e) {
+      // Handle Errors here.
+      const errorCode = e.code;
+      const errorMessage = e.message;
+      // The email of the user's account used.
+      const email = e.email;
+      // The firebase.auth.AuthCredential type that was used.
+      const credential = e.credential;
+
+      console.error({ errorCode, errorMessage, email, credential });
+    }
   }
 
+  async logout(): Promise<void> {
+    this.auth.signOut();
+  }
+}
+
+interface OAuthCredential {
+  accessToken: string;
+  providerId: string;
+  signInMethod: string;
+}
+
+interface Profile {
+  avatar_url: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,20 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAINESxOw7vphOe2caMdJfQb6zc2RlAjjc",
+  authDomain: "angular-i-like-it.firebaseapp.com",
+  projectId: "angular-i-like-it",
+  storageBucket: "angular-i-like-it.appspot.com",
+  messagingSenderId: "529526712534",
+  appId: "1:529526712534:web:473ce25d69df7ca8ba0177",
+  measurementId: "G-QGE12PS21D"
+};
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig
 };
 
 /*
