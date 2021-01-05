e26be4ec99c552c864f15162d4bdfa638a7d7cff
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-01-05 23:33:53 +0900

    公開しても問題ないが、作成し直したので消しておく

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAINESxOw7vphOe2caMdJfQb6zc2RlAjjc",
-  authDomain: "angular-i-like-it.firebaseapp.com",
-  projectId: "angular-i-like-it",
-  storageBucket: "angular-i-like-it.appspot.com",
-  messagingSenderId: "529526712534",
-  appId: "1:529526712534:web:473ce25d69df7ca8ba0177",
-  measurementId: "G-QGE12PS21D"
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 export const environment = {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,18 +4,18 @@
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAINESxOw7vphOe2caMdJfQb6zc2RlAjjc",
-  authDomain: "angular-i-like-it.firebaseapp.com",
-  projectId: "angular-i-like-it",
-  storageBucket: "angular-i-like-it.appspot.com",
-  messagingSenderId: "529526712534",
-  appId: "1:529526712534:web:473ce25d69df7ca8ba0177",
-  measurementId: "G-QGE12PS21D"
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 export const environment = {
   production: false,
-  firebaseConfig
+  firebaseConfig,
 };
 
 /*
