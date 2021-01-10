c5698bb8aa38c8c757f71f5e399fa6dc0d3d4f52
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-01-10 23:18:06 +0900

    add list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,28 @@
 import { LoginComponent } from './feature-modules/login/login.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { MainNavComponent } from './core/components/main-nav/main-nav.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: 'login',
     component: LoginComponent,
+    // canActivate: [],
   },
   {
     path: '',
     component: MainNavComponent,
+    // canActivate: [],
+    // canLoad: [],
     children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./feature-modules/list/list.module').then(
+            (m) => m.ListModule
+          ),
+      },
       {
         path: '',
         loadChildren: () =>
