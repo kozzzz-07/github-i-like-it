b3a5277f9c4ea443889fa5c4fde373cbcb1453fc
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-01-10 22:33:42 +0900

    疎通用

--- src/app/feature-modules/example/example.module.ts
+++ src/app/feature-modules/example/example.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExampleComponent } from './example.component';
 import { ExampleRoutingModule } from './example-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ExampleComponent],
   imports: [
     CommonModule,
     ExampleRoutingModule,
+    HttpClientModule
   ]
 
 })
