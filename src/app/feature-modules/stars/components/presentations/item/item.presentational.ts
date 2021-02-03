459d8558b391788243136c4431df4e35d586125f
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-02-03 23:47:00 +0900

    一旦 ItemPresentationalComponentへ移動

--- src/app/feature-modules/stars/components/presentations/item/item.presentational.ts
+++ src/app/feature-modules/stars/components/presentations/item/item.presentational.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-presentational-item',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.presentational.scss'],
 })
 export class ItemPresentationalComponent implements OnInit {
+  @Input() repository: any;
   constructor() {}
 
   ngOnInit(): void {}
