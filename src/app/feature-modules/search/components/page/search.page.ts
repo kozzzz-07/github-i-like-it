c94ca13c311bfcd5cf3440a40a72b91e131a66fe
kozzzz-07 <64407592+kozzzz-07@users.noreply.github.com>
2021-03-21 22:53:52 +0900

    add search page

---
+++ src/app/feature-modules/search/components/page/search.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  templateUrl: './search.page.html',
+  styleUrls: ['./search.page.scss']
+})
+export class SearchPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
