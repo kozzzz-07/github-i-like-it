9fdc95793e3d7a9f39285e8aa4111196d8622006
kozzzz-07 <kozzzz-07@users.noreply.github.com>
2021-03-23 23:07:30 +0900

    add subheader component

---
+++ src/app/feature-modules/search/components/presentations/subheader/subheader.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-presentational-subheader',
+  templateUrl: './subheader.component.html',
+  styleUrls: ['./subheader.component.scss'],
+})
+export class SubheaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/feature-modules/search/search.module.ts
+++ src/app/feature-modules/search/search.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SearchRoutingModule } from './search-routing.module';
 import { SearchPageComponent } from './components/page/search.page';
 import { ListContainerComponent } from './components/containers/list/list.container';
+import { ItemComponent } from './components/presentations/item/item.component';
+import { SubheaderComponent } from './components/presentations/subheader/subheader.component';
 
 @NgModule({
-  declarations: [SearchPageComponent, ListContainerComponent],
+  declarations: [
+    SearchPageComponent,
+    ListContainerComponent,
+    ItemComponent,
+    SubheaderComponent,
+  ],
   imports: [CommonModule, SearchRoutingModule],
 })
 export class SearchModule {}
